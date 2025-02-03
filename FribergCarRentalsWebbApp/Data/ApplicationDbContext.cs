a70fe1a8c77680b4633ad8e85e420ba2c8a9eb4a
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2025-02-03 11:46:43 +0100

    Implemented ApplicationDbContext

--- FribergCarRentalsWebbApp/Data/ApplicationDbContext.cs
+++ FribergCarRentalsWebbApp/Data/ApplicationDbContext.cs
@@ -1,8 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FribergCarRentalsWebbApp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace FribergCarRentalsWebbApp.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
     {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Administrator> Administrators { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Price> Prices { get; set; }
     }
 }
