bfd3983f831df43f372431c06c34184141d37d4a
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2025-02-03 18:31:42 +0100

    Implemented ICustomer

---
+++ FribergCarRentalsWebbApp/Data/ICustomer.cs
@@ -0,0 +1,13 @@
+﻿using FribergCarRentalsWebbApp.Models;
+
+namespace FribergCarRentalsWebbApp.Data
+{
+    public interface ICustomer
+    {
+        Customer? GetById(int id);
+        Customer? GetByCredentials(string email, string password);
+        void Add(Customer customer);
+        void Update(Customer customer);
+        IEnumerable<Customer> All();
+    }
+}
