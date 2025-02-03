4cde6cb10df04fa3eb949fdb95a5ec54fd0141bb
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2025-02-03 18:46:32 +0100

    Added AdministratorRepository

---
+++ FribergCarRentalsWebbApp/Data/AdministratorRepository.cs
@@ -0,0 +1,15 @@
+﻿using FribergCarRentalsWebbApp.Models;
+
+namespace FribergCarRentalsWebbApp.Data
+{
+    public class AdministratorRepository(ApplicationDbContext context) : IAdministrator
+    {
+        private readonly ApplicationDbContext _context = context;
+
+        public Administrator? GetByCredentials(string email, string password)
+        {
+            return _context.Administrators
+                .FirstOrDefault(c => c.Email == email && c.Password == password);
+        }
+    }
+}
