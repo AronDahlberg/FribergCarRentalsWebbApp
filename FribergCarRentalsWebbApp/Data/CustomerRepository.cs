13be13d81161ab59dd703e5c17f7630519e0f578
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2025-02-03 18:42:28 +0100

    Added CustomerRepository

---
+++ FribergCarRentalsWebbApp/Data/CustomerRepository.cs
@@ -0,0 +1,36 @@
+﻿using FribergCarRentalsWebbApp.Models;
+
+namespace FribergCarRentalsWebbApp.Data
+{
+    public class CustomerRepository(ApplicationDbContext context) : ICustomer
+    {
+        private readonly ApplicationDbContext _context = context;
+        public void Add(Customer customer)
+        {
+            _context.Add(customer);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<Customer> All()
+        {
+            return _context.Customers.AsEnumerable();
+        }
+
+        public Customer? GetByCredentials(string email, string password)
+        {
+            return _context.Customers
+                .FirstOrDefault(c => c.Email == email && c.Password == password);
+        }
+
+        public Customer? GetById(int id)
+        {
+            return _context.Customers.FirstOrDefault(c => c.Id == id);
+        }
+
+        public void Update(Customer customer)
+        {
+            _context.Update(customer);
+            _context.SaveChanges();
+        }
+    }
+}
