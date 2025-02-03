a4fccbf31b3ff78eb1383c0193037956ab3e641f
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2025-02-03 11:33:23 +0100

    Implemented account class

--- FribergCarRentalsWebbApp/Models/Account.cs
+++ FribergCarRentalsWebbApp/Models/Account.cs
@@ -2,6 +2,10 @@
 {
     public abstract class Account
     {
-
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public DateTime CreationDate { get; set; }
+        public bool Deleted { get; set; }
+        public DateTime? DeletionDate { get; set; }
     }
 }
