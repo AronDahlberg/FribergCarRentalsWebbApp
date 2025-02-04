f4e1baf789af263856c4eb7d902f3df996957ab9
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2025-02-04 19:04:06 +0100

    Implemented a cookie banner

---
+++ FribergCarRentalsWebbApp/wwwroot/js/cookieBanner.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $('#accept-cookies').click(function () {
+        // Set a session cookie so it doesn't show again in the current session
+        document.cookie = "cookiesAccepted=true; path=/; SameSite=Strict;";
+
+        $('#cookie-banner').fadeOut();
+    });
+});
