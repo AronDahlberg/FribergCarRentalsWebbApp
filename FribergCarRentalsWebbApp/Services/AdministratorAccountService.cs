3d17bd8efe0252eea783576ec7efbcfc64709f59
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2025-02-04 20:15:13 +0100

    Added IAdminstratorAccountService and AdministratorAccountService

---
+++ FribergCarRentalsWebbApp/Services/AdministratorAccountService.cs
@@ -0,0 +1,19 @@
+﻿using FribergCarRentalsWebbApp.Data;
+using FribergCarRentalsWebbApp.Models;
+
+namespace FribergCarRentalsWebbApp.Services
+{
+    public class AdministratorAccountService(IAdministrator administratorRepository) : IAdministratorAccountService
+    {
+        private readonly IAdministrator _administratorRepository = administratorRepository;
+        public Administrator? AuthenticateAccount(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ConfirmAccountAsAdministrator(int id)
+        {
+            return _administratorRepository.GetById(id) != null;
+        }
+    }
+}
---
+++ FribergCarRentalsWebbApp/Services/IAdministratorAccountService.cs
@@ -0,0 +1,10 @@
+﻿using FribergCarRentalsWebbApp.Models;
+
+namespace FribergCarRentalsWebbApp.Services
+{
+    public interface IAdministratorAccountService
+    {
+        Administrator? AuthenticateAccount(string email, string password);
+        bool ConfirmAccountAsAdministrator(int id);
+    }
+}
