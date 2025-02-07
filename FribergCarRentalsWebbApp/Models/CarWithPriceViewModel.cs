946eaff247395be437b7c09e13496b3f3e4b8976
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2025-02-07 18:35:00 +0100

    Added CarWithPriceViewModel

---
+++ FribergCarRentalsWebbApp/Models/CarWithPriceViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FribergCarRentalsWebbApp.Models
+{
+    public class CarWithPriceViewModel
+    {
+        public Car Car { get; set; }
+        public Price Price { get; set; }
+    }
+}
