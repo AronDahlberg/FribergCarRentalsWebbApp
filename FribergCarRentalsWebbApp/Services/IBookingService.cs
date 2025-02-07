0c12cd5558feeecfb7aeeafb197d6821226d9900
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2025-02-07 18:35:25 +0100

    Added booking service

---
+++ FribergCarRentalsWebbApp/Services/IBookingService.cs
@@ -0,0 +1,10 @@
+﻿using FribergCarRentalsWebbApp.Models;
+
+namespace FribergCarRentalsWebbApp.Services
+{
+    public interface IBookingService
+    {
+        IEnumerable<Car> GetAllCars();
+        Price GetCurrentCarPrice(Car car);
+    }
+}
