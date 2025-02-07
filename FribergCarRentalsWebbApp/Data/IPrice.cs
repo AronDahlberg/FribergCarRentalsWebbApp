ff48aca4a517b093a23276cf019aa35d838569b4
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2025-02-07 18:34:46 +0100

    Added IPrice and PriceRepository

---
+++ FribergCarRentalsWebbApp/Data/IPrice.cs
@@ -0,0 +1,15 @@
+﻿using FribergCarRentalsWebbApp.Models;
+using System.Linq.Expressions;
+
+namespace FribergCarRentalsWebbApp.Data
+{
+    public interface IPrice
+    {
+        Price? Get(int id);
+        Price? Find(Expression<Func<Price, bool>> predicate);
+        IEnumerable<Price> All();
+        void Add(Price price);
+        void Update(Price price);
+        void Save();
+    }
+}
---
+++ FribergCarRentalsWebbApp/Data/PriceRepository.cs
@@ -0,0 +1,38 @@
+﻿using FribergCarRentalsWebbApp.Models;
+using System.Linq.Expressions;
+
+namespace FribergCarRentalsWebbApp.Data
+{
+    public class PriceRepository : IPrice
+    {
+        public void Add(Price price)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Price> All()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Price? Find(Expression<Func<Price, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Price? Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Price price)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
