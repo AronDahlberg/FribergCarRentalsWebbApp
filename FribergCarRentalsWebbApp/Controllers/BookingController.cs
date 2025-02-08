40482c1289fbd086476384f09f2b312a93df0a8a
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2025-02-08 17:09:31 +0100

    Implemented http post Book and http get BookingConfirmation to BookingController

--- FribergCarRentalsWebbApp/Controllers/BookingController.cs
+++ FribergCarRentalsWebbApp/Controllers/BookingController.cs
@@ -1,12 +1,15 @@
-﻿using FribergCarRentalsWebbApp.Services;
+﻿using FribergCarRentalsWebbApp.Common;
+using FribergCarRentalsWebbApp.Models;
+using FribergCarRentalsWebbApp.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FribergCarRentalsWebbApp.Controllers
 {
-    public class BookingController(IBookingService bookingService, ICarService carService) : Controller
+    public class BookingController(IBookingService bookingService, ICarService carService, IAccountService accountService) : Controller
     {
         private readonly IBookingService _bookingService = bookingService;
         private readonly ICarService _carService = carService;
+        private readonly IAccountService _accountService = accountService;
 
         public IActionResult Index()
         {
@@ -18,6 +21,45 @@ namespace FribergCarRentalsWebbApp.Controllers
         public IActionResult Details(int id)
         {
             return View(_carService.GetById(id));
+        }
+
+        [HttpPost]
+        public IActionResult Book(int carId, string totalPrice, string pickupDateTime, string dropoffDateTime)
+        {
+            Car car = _carService.GetById(carId);
+
+            int userId = (int)(HttpContext.Items["UserId"] ?? throw new InvalidOperationException("Cannot create booking without available customer id"));
+            Customer customer = _accountService.GetCustomerById(userId) ?? throw new KeyNotFoundException($"Could not find customer with id: {userId}");
+
+            Booking booking = new()
+            {
+                BookingCreation = DateTime.UtcNow,
+                PickupDateTime = DateTime.Parse(pickupDateTime),
+                DropoffDateTime = DateTime.Parse(dropoffDateTime),
+                Car = car,
+                CustomerAccount = customer,
+                Invalidated = false,
+                Payments =
+                [
+                    new() {
+                        PaymentAmount = totalPrice,
+                        TypeOfPayment = PaymentType.Booking,
+                        ConfirmationDate = DateTime.UtcNow
+                    }
+                ]
+            };
+
+            _bookingService.CreateBooking(booking);
+
+            return RedirectToAction("BookingConfirmation", new { id = booking.Id });
+        }
+
+        [HttpGet]
+        public IActionResult BookingConfirmation(int id)
+        {
+            Booking booking = _bookingService.GetById(id) ?? throw new KeyNotFoundException($"Could not find booking with id: {id}");
+            
+            return View(booking);
         }
     }
 }
