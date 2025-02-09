111cab3fea88a6a838de9914cfe959841b974595
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2025-02-09 14:29:06 +0100

    Implemented editing email from account page

---
+++ FribergCarRentalsWebbApp/wwwroot/js/editEmail.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    $("#editEmailModal .btn-primary").click(function () {
+        var newEmail = $("#emailInput").val();
+
+        $.ajax({
+            url: "/Account/ChangeEmail",
+            type: "POST",
+            contentType: "application/json",
+            data: JSON.stringify({ email: newEmail }),
+            success: function (response) {
+                if (response.success) {
+                    $("#editEmailModal").modal("hide");
+                    location.reload();
+                } else {
+                    console.error("Email change error", response);
+                    alert("Error: " + response.message);
+                }
+            },
+            error: function (xhr, status, error) {
+                console.error("Email change error", xhr, status, error);
+                alert("An error occurred while updating the email.");
+            }
+        });
+    });
+});
