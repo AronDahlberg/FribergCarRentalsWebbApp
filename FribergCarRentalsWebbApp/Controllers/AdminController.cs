5f676e3c151d072eb7711312a831c285f72714e4
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2025-02-09 23:39:13 +0100

    Added login method for admin controller

--- FribergCarRentalsWebbApp/Controllers/AdminController.cs
+++ FribergCarRentalsWebbApp/Controllers/AdminController.cs
@@ -1,11 +1,13 @@
-﻿using FribergCarRentalsWebbApp.Services;
+﻿using FribergCarRentalsWebbApp.Models;
+using FribergCarRentalsWebbApp.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FribergCarRentalsWebbApp.Controllers
 {
-    public class AdminController(IAccountService accountService) : Controller
+    public class AdminController(IAccountService accountService, IAuthCookieService authCookieService) : Controller
     {
         private readonly IAccountService _accountService = accountService;
+        private readonly IAuthCookieService _authCookieService = authCookieService;
 
         public IActionResult Index()
         {
@@ -30,6 +32,20 @@ namespace FribergCarRentalsWebbApp.Controllers
         public IActionResult Bookings()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string email, string password)
+        {
+            Administrator? admin = _accountService.AuthenticateAdministratorAccount(email, password);
+            if (admin == null)
+            {
+                return Unauthorized("Invalid credentials");
+            }
+
+            _authCookieService.CreateAdministratorAuthCookie(admin);
+
+            return Json(new { success = true, message = "Login successful." });
         }
     }
 }
