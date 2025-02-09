1428a5954ffdca5d57496b441bb72775971144fc
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2025-02-09 23:22:43 +0100

    Implemented unatuhorized check in AdminController for Index

--- FribergCarRentalsWebbApp/Controllers/AdminController.cs
+++ FribergCarRentalsWebbApp/Controllers/AdminController.cs
@@ -1,11 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FribergCarRentalsWebbApp.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FribergCarRentalsWebbApp.Controllers
 {
-    public class AdminController : Controller
+    public class AdminController(IAccountService accountService) : Controller
     {
+        private readonly IAccountService _accountService = accountService;
+
         public IActionResult Index()
         {
+            int userId = (int)(HttpContext.Items["UserId"] ?? 0);
+            bool userIsAdmin = (bool)(HttpContext.Items["UserAdmin"] ?? false);
+
+            // If logged in as either someone not marked an admin,
+            // or marked as one but has an invalid admin account
+            if (userId != 0 && (!_accountService.ConfirmAccountAsAdministrator(userId) || !userIsAdmin))
+            {
+                return Unauthorized();
+            }
+
             return View();
         }
 
