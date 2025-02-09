8c726dcca724a2889d1b0a847bf05c59810716d2
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2025-02-09 22:44:58 +0100

    Added AdminController with relevant views

---
+++ FribergCarRentalsWebbApp/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FribergCarRentalsWebbApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Customers()
+        {
+            return View();
+        }
+
+        public IActionResult Bookings()
+        {
+            return View();
+        }
+    }
+}
