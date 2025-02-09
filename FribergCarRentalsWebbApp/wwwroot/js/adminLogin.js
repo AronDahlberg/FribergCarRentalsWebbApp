df4fcb54158aa927498f9043298dc090509571bd
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2025-02-09 23:37:29 +0100

    Added js for admin login

---
+++ FribergCarRentalsWebbApp/wwwroot/js/adminLogin.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+    $("#admin-login-form").submit(function (e) {
+        e.preventDefault();
+
+        var formData = {
+            email: $("#adminLoginEmail").val(),
+            password: $("#adminLoginPassword").val()
+        };
+
+        $.ajax({
+            type: "POST",
+            url: "/Admin/Login",
+            data: formData,
+            dataType: "json",
+            success: function (response) {
+                location.reload();
+            },
+            error: function (xhr, status, error) {
+                if (xhr.status === 401) {
+                    alert("Invalid email or password")
+                } else {
+                    console.error("Login error", xhr, status, error);
+                    alert("An unexpected error occurred. Please try again later.");
+                }
+            }
+        });
+    });
+});
