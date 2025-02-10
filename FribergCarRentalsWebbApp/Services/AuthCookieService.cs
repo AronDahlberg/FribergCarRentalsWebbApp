40bb5b035c7035f8fd7f1c785e35f106727f2b5f
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2025-02-10 01:08:22 +0100

    Implemented CreateAdministratorAuthCookie in AuthCookieService

--- FribergCarRentalsWebbApp/Services/AuthCookieService.cs
+++ FribergCarRentalsWebbApp/Services/AuthCookieService.cs
@@ -26,6 +26,26 @@ namespace FribergCarRentalsWebbApp.Services
             _httpContextAccessor.HttpContext.Response.Cookies.Append("UserAuth", cookieValue, cookieOptions);
         }
 
+        public void CreateAdministratorAuthCookie(Administrator administrator)
+        {
+            if (_httpContextAccessor == null || _httpContextAccessor.HttpContext == null)
+            {
+                throw new Exception("Unexpected error: IHttpContextAccesor.HttpContext cannot be null.");
+            }
+
+            // Create a cookie with format: "UserId|IsAdmin"
+            bool isAdmin = true;
+            string cookieValue = $"{administrator.Id}|{isAdmin.ToString().ToLower()}";
+            CookieOptions cookieOptions = new()
+            {
+                HttpOnly = true,
+                Secure = _httpContextAccessor.HttpContext.Request.IsHttps
+                // Session cookie: no set expiration.
+            };
+
+            _httpContextAccessor.HttpContext.Response.Cookies.Append("UserAuth", cookieValue, cookieOptions);
+        }
+
         public void DeleteUserAuthCookie()
         {
             if (_httpContextAccessor == null || _httpContextAccessor.HttpContext == null)
--- FribergCarRentalsWebbApp/Services/IAuthCookieService.cs
+++ FribergCarRentalsWebbApp/Services/IAuthCookieService.cs
@@ -5,6 +5,7 @@ namespace FribergCarRentalsWebbApp.Services
     public interface IAuthCookieService
     {
         void CreateCustomerAuthCookie(Customer customer);
+        void CreateAdministratorAuthCookie(Administrator administrator);
         void DeleteUserAuthCookie();
     }
 }
